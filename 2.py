ae2a460b2b8cf5eca48f5f9748f514d465e82a10
tdquang17a3 <148149038+WangZ05@users.noreply.github.com>
2023-11-16 21:24:29 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+x = 10
+y = 5
+
+def function(x,y):   
+    print("x=10,y=5")
+    print("Tong: x + y =", x + y)
+    print("Hieu: x - y =", x - y)
+    print("Tich: x * y =", x * y)
+    print("Thuong: x / y =", x / y)
+function(x,y)
